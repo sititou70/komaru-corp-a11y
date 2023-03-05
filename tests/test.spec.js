231c85b27d7044bce6205ce6a824aaa45d16ad4a
Takuya Shizukuishi <sititou70@gmail.com>
2023-03-05 16:38:57 +0900

    fix: テストを改善

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -2,15 +2,14 @@ import { test, expect } from "@playwright/test";
 
 test("ナビゲーションメニューを開ける", async ({ page }) => {
   await page.goto("/");
-  await page.locator(".nav_open button").dispatchEvent("click");
-  await expect(page.locator(".navigation .container")).toHaveCSS(
-    "display",
-    "block"
+  await page.getByRole("button", { name: /MENU/i }).dispatchEvent("click");
+  await expect(page.getByRole("dialog", { name: /MENU/i })).toHaveAttribute(
+    "open"
   );
 });
 
 test("アンケート：氏名は任意要素", async ({ page }) => {
   await page.goto("/");
-  const input = page.locator(".campaign form input[name=name]");
+  const input = page.getByRole("textbox", { name: /氏名/i });
   await expect(input).not.toHaveAttribute("required");
 });
